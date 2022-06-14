973884cf0c1b4ebc0c13aefaedc8181d09ea4e18
Sergio473 <sergioabisay5@gmail.com>
2022-06-14 11:38:59 -0500

    El archivo esta listo pa subirse

---
+++ basico/modulo2/2.2.1.11.py
@@ -0,0 +1,5 @@
+''' dentro del print se mete lo que queremos mostrar 
+en comillas simples, y se usa una diagonal invertida
+para que me muestre el resultado con las comillas dobles '''
+
+print('\"Estoy"', '\""aprendiendo""', '\"""Python"""' ,sep="\n", )
---
+++ basico/modulo2/2.4.1.7.py
@@ -0,0 +1,12 @@
+juan = 3
+maria= 5
+adan=6
+
+print(juan, maria, adan)
+total_manzanas=(juan+maria+adan)
+print('el total es '+ str(total_manzanas))
+
+'''
+// significa que dividira numeros enteros
+/ significa que dividira con numeros decimales
+'''
